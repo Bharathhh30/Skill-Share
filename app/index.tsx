3b54ec7ae1d5a96d84e912ffbc9afb4f6d9ede9c
Bharathhh30 <vnsb30@gmail.com>
2025-03-31 14:45:29 +0530

    @0 setup with nativewind and preset(this commit is a nativewind template app)

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-black text-3xl font-bold">Onto The new app : SKILL SHARE</Text>
+    </View>
+  );
+}
